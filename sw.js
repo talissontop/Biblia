5da553dc8cf043842470c4435efcbd3416132257
talisson <72979419+talissontop@users.noreply.github.com>
2026-07-31 19:31:05 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿const CACHE_NAME = 'biblia-3d-cache-v1';
+const urlsToCache = [
+    './index.html',
+    './js/core/app.js',
+    './js/engine3d/scene.js',
+    './js/components/ui.js',
+    './data/verses/biblia_completa.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
